b36e3087bb6084becdc19e0780134594c840bf73
DBHow <65567607+DBHow@users.noreply.github.com>
2023-05-02 00:49:44 -0500

    Create script_file.py

---
+++ scripts/script_file.py
@@ -0,0 +1,6 @@
+class script_file:
+    def __init__(self, name, order, description, filepath, deployed_date=None, deployed_status=None):
+        self.name = name
+        self.order = order
+        self.description = description
+        self.filepath = filepath
